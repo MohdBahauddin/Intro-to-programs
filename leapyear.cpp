cf0249074e97187db9c848e7c3532b91a6e6c722
MohdBahauddin <92330584+MohdBahauddin@users.noreply.github.com>
2022-09-18 16:42:50 +0530

    Create leapyear.cpp

---
+++ leapyear.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int year;
+    cout<<"enter a year "<<endl;
+    cin>>year;
+    if(year%4==0 && year%100!=0|| year%400==0)
+    {
+        cout<<"year is leap year "<<endl;
+    }
+    else
+    {
+        cout<<"year is not leap year ";
+    }
+}
